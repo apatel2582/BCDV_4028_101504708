e31918b020342be7a8a7212cc71b8140685a8a4e
Anishkumar Patel <126298288+apatel2582@users.noreply.github.com>
2023-09-23 16:00:32 -0400

    Lab 02 Task 02
    BCDV 4028 - Lab 02 Task 02

---
+++ Lab_02_4028_Task_02.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// NOTE: Deploy this contract (UpdaterContract) first
+contract UpdaterContract {
+    // NOTE: storage layout must be the same as StudentMarks contract
+    uint public marks;
+    address public studentAddress;
+
+    function updateStudentMarks(uint _marks) public {
+        marks = _marks;
+        studentAddress = msg.sender;
+    }
+}
+
+contract StudentMarks {
+    uint public marks;
+    address public studentAddress;
+
+    function updateMarksViaDelegateCall(address _updaterContract, uint _marks) public {
+    // StudentMarks' storage is set, UpdaterContract is not modified.
+        (bool success, ) = _updaterContract.delegatecall(
+            abi.encodeWithSignature("updateStudentMarks(uint256)", _marks)
+        );
+        require(success, "Delegatecall failed.");
+    }
+
+}
